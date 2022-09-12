02042a8c481cc7267f15063b71b26de64d546e16
MorganTwoZero <90017582+MorganTwoZero@users.noreply.github.com>
2022-09-12 14:27:02 +0200

    Fixed keep_clean

--- src/cleaning_cog.py
+++ src/cleaning_cog.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 
 from discord import ApplicationContext, Bot, Cog, slash_command, Interaction, TextChannel
@@ -10,16 +11,8 @@ class Cleaning(Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self._channels: list[TextChannel] = []
+        self._channels: set[TextChannel] = set()
         self.clean.start()
-
-    @property
-    def channels(self):
-        return self._channels
-
-    @channels.setter
-    def channels(self, channel):
-        self._channels.append(channel)
 
     @slash_command()
     async def keep_clean(
@@ -30,15 +23,20 @@ class Cleaning(Cog):
         assert isinstance(ctx.channel, TextChannel)
         logger.info('Channel "%s" saved for cleaning', ctx.channel.name)
 
+        self._channels.add(ctx.channel)
+
         response = await ctx.respond(f'Оке, удаляю всё старше одного дня')
         assert isinstance(response, Interaction)
         await response.delete_original_message(delay=5)
 
-    @tasks.loop(hours=24)
+    @tasks.loop(minutes=5)
     async def clean(self):
-        for channel in self.channels:
+        for channel in self._channels:
             logger.info('Cleaning channel "%s"', channel.name)
-            await channel.purge()
+
+            ttl = datetime.datetime.now() - datetime.timedelta(days=1)
+            res = await channel.purge(before=ttl)
+            logger.debug("Deleted %s messages", len(res))
 
     @slash_command(name='purge')
     async def purge(self, ctx: ApplicationContext, limit: int):
